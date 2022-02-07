2f090410a054fe16d782361d241990a6b7bf502e
Harshil Khamar <harshilkhamar1@gmail.com>
2022-02-08 01:28:49 +0530

    menu added

---
+++ item-menu.php
@@ -0,0 +1,127 @@
+<div class="container">
+<nav>
+  <div class="nav nav-tabs nav-justified" id="nav-tab" role="tablist">
+    <button class="nav-link active" id="nav-laddu-tab" data-bs-toggle="tab" data-bs-target="#nav-laddu" type="button" role="tab" aria-controls="nav-laddu" aria-selected="true">Laddu</button>
+    <button class="nav-link" id="nav-snacks-tab" data-bs-toggle="tab" data-bs-target="#nav-snacks" type="button" role="tab" aria-controls="nav-snacks" aria-selected="false">Snacks</button>
+    <button class="nav-link" id="nav-papad-tab" data-bs-toggle="tab" data-bs-target="#nav-papad" type="button" role="tab" aria-controls="nav-papad" aria-selected="false">Papad</button>
+  </div>
+</nav>
+<div class="tab-content" id="nav-tabContent">
+    <div class="tab-pane fade show active" id="nav-laddu" role="tabpanel" aria-labelledby="nav-laddu-tab">
+      <div class="container d-flex justify-content-center flex-wrap my-4">
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="nav-snacks" role="tabpanel" aria-labelledby="nav-snacks-tab">
+     <div class="container d-flex justify-content-center flex-wrap my-4">
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+            </div>
+        </div>
+        <div class="card m-4" style="width:16rem;">
+            <img src="Assets/Images/slider_1.jpg" class="card-img-top">
+            <div class="card-body">
+            <h5 class="card-title">Mava Chikki</h5>
+            <p class="card-text mb-0">Price : 120 Rs / Pkt</p>
+            <p class="card-text mt-0">Quantity : 500 gm</p>
+            <a href="#" class="btn btn-primary">Buy Now <i class="fas fa-shopping-cart"></i></a>
+        </div>
+    </div>
+  </div>
+  <div class="tab-pane fade" id="nav-papad" role="tabpanel" aria-labelledby="nav-papad-tab">...</div>
+</div>
+</div>
