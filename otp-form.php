c71f082611cfc84037de6c7804aa20a8c10e7926
Astha1404 <patelastha252@gmail.com>
2022-02-14 22:11:12 +0530

    smtp

--- otp-form.php
+++ otp-form.php
@@ -1,38 +1,40 @@
 <?php
-require 'Assets/Mailer/PHPMailer.php';
-require 'Assets/Mailer/SMTP.php';
-require 'Assets/Mailer/Exception.php';
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+    require 'Assets/Mailer/PHPMailer.php';
+    require 'Assets/Mailer/SMTP.php';
+    require 'Assets/Mailer/Exception.php';
+    
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\SMTP;
+    use PHPMailer\PHPMailer\Exception;
+    if(!isset($_GET['mailerror']))
+    {
+    $mail = new PHPMailer;
 
-$mail = new PHPMailer;
+    //$mail->SMTPDebug = 3;                               // Enable verbose debug output
 
-//$mail->SMTPDebug = 3;                               // Enable verbose debug output
+    $mail->isSMTP();                                      // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                               // Enable SMTP authentication
+    $mail->Username = 'patelastha252@gmail.com';                 // SMTP username
+    $mail->Password = 'Rovina3197@';                           // SMTP password
+    $mail->SMTPSecure = 'ssl';                              // Enable TLS encryption, `ssl` also accepted
+    $mail->Subject = "OTP";
+    $mail->Port = 587;                                    // TCP port to connect to
+    
+    
+    $mail->setFrom('patelastha252@gmail.com', 'Charvi');
+    $mail->addAddress("{$_SESSION['Remail']}", "{$_SESSION['Rname']}");     // Add a recipient     // Name is optional
+    $mail->addReplyTo('no-reply@gmail.com', 'Do Not Reply');
+    $mail->isHTML(true);                                  // Set email format to HTML
+    
+    $mail->Subject = 'OTP';
+    $mail->Body = "Your OTP for Charvi : {$_SESSION['randomOTP']}";
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'patelastha252@gmail.com';                 // SMTP username
-$mail->Password = '';                           // SMTP password
-$mail->SMTPSecure = 'tls';                              // Enable TLS encryption, `ssl` also accepted
-$mail->Body = "Your OTP for Charvi : {$_SESSION['randomOTP']}";
-$mail->Port = 587;                                    // TCP port to connect to
+    if(!$mail->send()) {
+        $error='Message could not be sent.'.'\nMailer Error: ' . $mail->ErrorInfo;
+        header("Location: /charvi/register.php?error={$error} & otperror=1 & mailerror=1");
 
-
-$mail->setFrom('patelastha252@gmail.com', 'Charvi');
-$mail->addAddress("{$_SESSION['Remail']}", "{$_SESSION['Rname']}");     // Add a recipient     // Name is optional
-$mail->addReplyTo('no-reply@gmail.com', 'Do Not Reply');
-$mail->isHTML(true);                                  // Set email format to HTML
-
-$mail->Subject = 'Here is the subject';
-$mail->Body    = 'This is the HTML message body <b>in bold!</b>';
-$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-if(!$mail->send()) {
-    $error='Message could not be sent.'.'\nMailer Error: ' . $mail->ErrorInfo;
-    header("Location: /charvi/register.php?error={$error} & otperror=1");
-
+    }
 }
 ?>
 
